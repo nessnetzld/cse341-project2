85808cd589977a7c3a04b97b341a5c108e278432
Nefi Zaldana <nefizaldana@gmail.com>
2025-09-14 20:30:22 -0600

    swagger installation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
+  // #swagger.tags=['Hello World']
   res.send("Hello, World!");
 });
 
 router.use("/users", require("./users"));
+router.use("/", require("./swagger"));
 
 module.exports = router;
