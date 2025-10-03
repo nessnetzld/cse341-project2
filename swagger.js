062bd1d47b95cb9b003ef9126340de13c7b3b102
Nefi Zaldana <nefizaldana@gmail.com>
2025-10-02 23:34:06 -0600

    deploy

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: "Users Api",
     description: "Users Api",
   },
-  host: "localhost:3001",
-  schemes: ["http", "https"],
+  host: "cse341-project2-teey.onrender.com",
+  schemes: ["https"],
 };
 
 const outputFile = "./swagger.json";
