23c588f7218d871c84b79588c5937f69a851ae4c
Nefi Zaldana <nefizaldana@gmail.com>
2025-10-04 11:03:49 -0600

    login

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,24 @@
 const router = require("express").Router();
+const passport = require("passport");
 
-router.get("/", (req, res) => {
+/* router.get("/", (req, res) => {
   // #swagger.tags=['Hello World']
   res.send("Hello, World!");
 });
-
+ */
 router.use("/users", require("./users"));
 router.use("/contacts", require("./contacts"));
 router.use("/", require("./swagger"));
+
+router.get("/login", passport.authenticate("github"), (req, res) => {});
+
+router.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongodb = require("./data/database");
-const app = express();
+const session = require("express-session");
+const GitHubStrategy = require("passport-github2").Strategy;
+const passport = require("passport");
+const cors = require("cors");
 
+const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
+// Basic express basic session initialization
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize()); // init passport on every route call
+app.use(passport.session()); // allow passport to use "express-session".
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -18,7 +34,59 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+// Middleware
+app.use(cors({ methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"] }));
+app.use(cors({ origin: "*" }));
 app.use("/", require("./routes"));
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      // User.findOrCreate({ githubIs: profile.id }, function (err, user) {
+      return done(null, profile);
+      //})
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get("/", (req, res) => {
+  if (req.session.user != undefined) {
+    const name =
+      req.session.user.displayName || req.session.user.username || "Unknown";
+    res.send(`Logged in as ${name}`);
+  } else {
+    res.send("Logged Out");
+  }
+});
+
+app.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: "/api-docs",
+  }),
+  (req, res) => {
+    req.session.user = {
+      displayName: req.user.displayName,
+      username: req.user.username,
+      id: req.user.id,
+      profileUrl: req.user.profileUrl,
+    };
+    res.redirect("/");
+  }
+);
 
 process.on("uncaughtException", (err, origin) => {
   console.log(
